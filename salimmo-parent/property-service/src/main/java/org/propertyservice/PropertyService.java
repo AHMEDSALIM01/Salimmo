32666a34fd60c2ad8934a3f5e1613b10ecf0ec81
Ahmed Salim <93927978+AHMEDSALIM01@users.noreply.github.com>
2023-03-12 20:05:51 +0100

    creation branch develop

---
+++ salimmo-parent/property-service/src/main/java/org/propertyservice/PropertyService.java
@@ -0,0 +1,11 @@
+package org.propertyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyService {
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyService.class,args);
+    }
+}
