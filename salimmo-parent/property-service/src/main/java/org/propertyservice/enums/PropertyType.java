5c5a3ae052476f222bc864323ff1e0df3eaac546
Ahmed Salim <93927978+AHMEDSALIM01@users.noreply.github.com>
2023-04-05 09:43:48 +0000

    creation enum property-type.ts

--- salimmo-parent/property-service/src/main/java/org/propertyservice/enums/PropertyType.java
+++ salimmo-parent/property-service/src/main/java/org/propertyservice/enums/PropertyType.java
@@ -1,4 +1,17 @@
 package org.propertyservice.enums;
 
 public enum PropertyType {
+    APARTMENT,
+    DUPLEX,
+    VILLA,
+    HOUSE,
+    BUILDING,
+    GROUND,
+    BUNGALOW,
+    COTTAGE,
+    FACTORY,
+    RIAD,
+    TRIPLEX,
+    STUDIO,
+    HANGAR
 }
